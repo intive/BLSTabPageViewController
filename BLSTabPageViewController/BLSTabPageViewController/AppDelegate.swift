f8e4fce374bd73e41854438326ead19bc18f4fa6
Paweł Knuth <pawel.knuth@blstream.com>
2016-06-01 11:22:08 +0200

    [ADD] Initial project creation

---
+++ BLSTabPageViewController/BLSTabPageViewController/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BLSTabPageViewController
+//
+//  Created by Paweł Knuth on 01.06.2016.
+//  Copyright © 2016 Paweł Knuth. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ BLSTabPageViewController/BLSTabPageViewController/BLSTabPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BLSTabPageViewController.swift
+//  BLSTabPageViewController
+//
+//  Created by Paweł Knuth on 01.06.2016.
+//  Copyright © 2016 Paweł Knuth. All rights reserved.
+//
+
+import UIKit
+
+class BLSTabPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
